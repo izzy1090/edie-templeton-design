588ef21db33592f85c5b592a364653b90477d799
Michael Isabella <ediesnyder@Izzys-MBP.lan>
2023-09-26 23:40:57 -0400

    Added a hamburger menu that positions nav menu elements in center

--- src/components/NavMenu.js
+++ src/components/NavMenu.js
@@ -7,10 +7,6 @@ function NavMenu( { hamburger, invertedHamburger, activeMenu } ){
         setIsOpen(!isOpen)  
     }
 
-    // If the isOpen state is set to true
-        // create a menu in the center of the screen
-            // the menu should navigate around the site (i.e. home, work, contact, etc)
-    // else display the homepage and navMenu as is
     if (isOpen) {
         return <>
             <div onClick={handleClick}>{invertedHamburger}</div>
