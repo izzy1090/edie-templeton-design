cc8ec1dcc481737d81fb6c088ab38e5d93111074
Michael Isabella <ediesnyder@Izzys-MBP.lan>
2023-09-29 14:48:54 -0400

    Added functionality to close menu when the user clicks outside of a nav link

--- src/components/NavMenu.js
+++ src/components/NavMenu.js
@@ -1,10 +1,30 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function NavMenu( { hamburger, invertedHamburger, activeMenu } ){
     const [isOpen, setIsOpen] = useState(false);
-    
+
+        // useEffect so that the web page is always looking for the click event
+        useEffect(()=>{
+            // sets up a click handler and passes in a click event
+            const handler = (event) => {
+                // init. a var to look for the click event target's className
+                const clickedDev = event.target.className;
+                // if the class name is the activeMenu
+                if (clickedDev === 'activeMenu'){
+                    // close the menu
+                    setIsOpen(false)
+                }
+            }
+            
+            document.addEventListener('click', handler)
+            return ()=>{
+                document.removeEventListener('click', handler)
+            }
+            
+        },[])
+
     const handleClick = () => {
-        setIsOpen(!isOpen)  
+        setIsOpen(!isOpen);  
     }
 
     if (isOpen) {
