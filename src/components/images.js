aae520455a8f0b1175a4adaf5d0d2d5d7a345fdd
izzy1090 <michaelisabella1@gmail.com>
2023-10-30 19:19:57 -0400

    Added an images component and abstracted code for readability
    Specifically I used .map and .filter to split up all of the images
    into two separate columns to allow us to have individual indices
    associated with each individual image.

--- src/components/images.js
+++ src/components/images.js
@@ -1,7 +1,34 @@
-function Images ( {images} ) {
-    // Leave component for now in case you decide to add any functionality
+import useGlobalStates from '../hooks/use-globalStates';
 
-    return <> {images} </>
+function Images ( {images} ) {
+
+    const { isOpen } = useGlobalStates();
+
+    const imageWidth = 1000;
+
+    // if an image's index is less than 7, map it to the first imageContainerColumn div
+    const imageContainerColumn1 = images.filter((image, i)=> i < 7).map((image, i)=>{
+        return <img src={image} key={i} alt='' width={imageWidth}></img>
+    })
+    // Uncomment to see what images are in the below container
+    // console.log(imageContainerColumn1)
+
+    // if an image's index is greater than or equal to 7, map it to the first imageContainerColumn div
+    const imageContainerColumn2 = images.filter((image, i)=> i >= 7).map((image, i)=>{
+        return <img src={image} key={i} width={imageWidth} alt=''></img>
+    })
+
+    // Uncomment to see what images are in the below container
+    // console.log(imageContainerColumn2)
+
+    return <div className={isOpen ? "imagesContainer" : "imagesContainer beforeHover"}>
+        <div className="imageContainerColumn1">
+            {imageContainerColumn1}
+        </div>
+        <div className="imageContainerColumn2">
+            {imageContainerColumn2}
+        </div>
+    </div>
 
 }
 
--- src/pages/ImagesPage.js
+++ src/pages/ImagesPage.js
@@ -1,3 +1,4 @@
+import Images from '../components/images';
 import grammieKitchen from '../images/grammies-kitchen.jpg';
 import chineseCabinet from '../images/chinese-cabinet.jpg';
 import livingRoom1 from '../images/living-room1.jpg';
@@ -12,39 +13,12 @@ import bedroom2 from '../images/bedroom2.jpg';
 import nightstand1 from '../images/nightstand1.jpg';
 import nightstand2 from '../images/nightstand2.jpg';
 import NYT from '../images/NYT.jpg';
-import useGlobalStates from '../hooks/use-globalStates';
-
 
 function ImagesPage () {
 
-    const { isOpen } = useGlobalStates();
+    const imageArray = [grammieKitchen, chineseCabinet, livingRoom1, livingRoom2, livingRoom3, bathroom1, bathroom2, flowers, diningRoom, bedroom1, bedroom2, nightstand1, nightstand2, NYT]
 
-    const imageWidth = 1000;
-
-    const images = <>
-        <div className={isOpen ? "imagesContainer" : "imagesContainer beforeHover"}> 
-            <div className="imageContainerColumn1">
-                <img src={grammieKitchen} width={imageWidth} alt=""></img>
-                <img src={chineseCabinet} width={imageWidth} alt=""></img>
-                <img src={livingRoom1} width={imageWidth} alt=""></img>
-                <img src={livingRoom2} width={imageWidth} alt="" loading='lazy'></img>
-                <img src={livingRoom3} width={imageWidth} alt="" loading='lazy'></img>
-                <img src={bathroom1} width={imageWidth} alt="" loading='lazy'></img>
-                <img src={bathroom2} width={imageWidth} alt="" loading='lazy'></img>
-            </div>
-            <div className="imageContainerColumn2">
-                <img src={flowers} width={imageWidth} alt=""></img>
-                <img src={diningRoom} width={imageWidth} alt=""></img>
-                <img src={bedroom1} width={imageWidth} alt=""></img>
-                <img src={bedroom2} width={imageWidth} alt="" loading='lazy'></img>
-                <img src={nightstand1} width={imageWidth} alt="" loading='lazy'></img>
-                <img src={nightstand2} width={imageWidth} alt="" loading='lazy'></img>
-                <img src={NYT} width={imageWidth} alt="" loading='lazy'></img>
-            </div>
-        </div>
-    </>
-
-    return <>{ images }</>
+    return <Images images={imageArray}/>
 }
 
 export default ImagesPage;
