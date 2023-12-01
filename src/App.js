1daf9b69ef65e9c8461d871871c6d2e9f4e78a58
Michael Isabella <ediesnyder@Izzys-MBP.lan>
2023-11-30 23:04:05 -0500

    Added a lazy loading feature to test for a bit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import Route from './components/route';
 import NavMenuPage from "./pages/NavMenuPage";
-import ImagesPage from "./pages/ImagesPage";
+// import ImagesPage from "./pages/ImagesPage";
 import AboutPage from "./pages/AboutPage";
 import ContactPage from "./pages/ContactPage";
+import { Suspense, lazy } from "react";
+
+const ImagesPage = lazy(() => import('./pages/ImagesPage'))
 
 function App() {
   return (
   <>
     <Route path="/">
       <NavMenuPage/>
-      <ImagesPage/>
+        <Suspense fallback={<div>Component1 are loading please wait...</div>}>
+        <ImagesPage/>
+      </Suspense>
     </Route>
     <Route path="/about">
       <NavMenuPage/>
