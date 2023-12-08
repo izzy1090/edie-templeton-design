68dbf72f91f59f409aa973c465d18627c30d9304
Michael Isabella <ediesnyder@Izzys-MBP.lan>
2023-12-07 19:21:19 -0500

    Renamed a variable

--- src/components/navMenu.js
+++ src/components/navMenu.js
@@ -10,9 +10,9 @@ function NavMenu( { hamburger, invertedHamburger, activeMenu } ){
             // sets up a click handler and passes in a click event
             const handler = (event) => {
                 // init. a var to look for the click event target's className
-                const clickedDev = event.target.className;
+                const clickedDiv = event.target.className;
                 // if the class name is the activeMenu
-                if (clickedDev === 'activeMenu'){
+                if (clickedDiv === 'activeMenu'){
                     // close the menu
                     setIsOpen(false)
                 }
