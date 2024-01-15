e6d6e98be4451f7cb493cc501b899694be1326a1
izzy1090 <michaelisabella1@gmail.com>
2024-01-15 17:15:16 -0500

    Added a desktop menu and a mobile nav menu

--- src/components/navMenu.js
+++ src/components/navMenu.js
@@ -1,42 +1,61 @@
 import { useEffect } from "react";
 import useGlobalStates from "../hooks/use-globalStates";
 
-function NavMenu( { hamburger, invertedHamburger, activeMenu } ){
+function NavMenu( { menuItems } ){
 
     const { isNavOpen, setIsOpen } = useGlobalStates();
 
-        // useEffect so the web page is always looking for a click event
-        useEffect(()=> {
+    const hamburger = <div className="hamburger"></div>
+    const invertedHamburger = <div className="invertedHamburger"></div>
+
+    // useEffect so the web page is always looking for a click event
+    useEffect(()=> {
+        if (isNavOpen){
             // sets up a click handler and passes in a click event
             const handler = (event) => {
                 // init. a var to look for the click event target's className
                 const clickedDiv = event.target.className;
                 // if the class name is the activeMenu
-                if (clickedDiv === 'activeMenu'){
+                if (clickedDiv === 'activeMenu' || event.key === 'Escape'){
                     // close the menu
-                    setIsOpen(false)
-                }
+                    setIsOpen(false);
+                } 
             }
-
-            document.addEventListener('click', handler)
+        
+            document.addEventListener('click', handler);
+            document.addEventListener('keydown', handler);
 
             return ()=>{
                 document.removeEventListener('click', handler)
+                document.removeEventListener('keydown', handler);
             }
-            
-        }, [setIsOpen])
+        }
+    }, [setIsOpen, isNavOpen])
 
     const handleOpenCloseMenu = () => {
         setIsOpen(!isNavOpen)
     }
+
+    const mobileActiveMenu = menuItems.map((menuItem)=>{
+        return <div key={menuItem.key}>
+            <a href={menuItem.path}>{menuItem.value}</a>
+        </div>
+    })
+
+    const renderedMenuItems = menuItems.map((menuItem)=>{
+        return <div key={menuItem.key} style={{padding: '10px'}}>
+            <a href={menuItem.path}>{menuItem.value}</a>
+        </div>
+    })
 
     return <>
         <div className={isNavOpen ? "menuTransitionIn" : "menuTransitionOut"}>
         <span onClick={handleOpenCloseMenu}>
             {isNavOpen ? invertedHamburger : hamburger}
         </span>
-            {isNavOpen ? activeMenu : null}
+            {isNavOpen ? <div className="mobileActiveMenu">{mobileActiveMenu}</div> : null}
         </div>
+        <div className="desktopMenu">{renderedMenuItems}</div>
     </>
 }
 
--- src/pages/ImagesPage.js
+++ src/pages/ImagesPage.js
@@ -35,20 +35,20 @@ import NYTCompressed from '../images/gallery/NYT.webp';
 function ImagesPage () {
 
     const imageArray = [ 
-        {key: 0, highResImage: grammieKitchen, compressedImage: grammieKitchenCompressed, alt: "Edie's grandma's kitchen." , loading: 'lazy'}, 
-        {key: 1, highResImage: chineseCabinet, compressedImage: chineseCabinetCompressed, alt: 'Ornate black and gold chinese cabinet.', loading: 'lazy'}, 
-        {key: 2, highResImage: livingRoom1, compressedImage: livingRoom1Compressed, width: 1800, height: 1200, alt: '72 Clermont Ave shot # 1.', loading: 'lazy'}, 
-        {key: 3, highResImage: livingRoom2, compressedImage: livingRoom2Compressed, width: 1200, height: 1800, alt: '72 Clermont Ave shot # 2.', loading: 'lazy' }, 
-        {key: 4, highResImage: livingRoom3, compressedImage: livingRoom3Compressed, width: 1200, height: 1800, alt: '72 Clermont Ave shot # 3.', loading: 'lazy'}, 
-        {key: 5, highResImage: bathroom1, compressedImage: bathroom1Compressed, width: 1300, height: 1950, alt: 'Bathroom shot 1.', loading: 'lazy'}, 
-        {key: 6, highResImage: bathroom2, compressedImage: bathroom2Compressed, width: 1300, height: 1950, alt: 'Bathroom shot 2.', loading: 'lazy'}, 
-        {key: 7, highResImage: flowers, compressedImage: flowersCompressed, width: 1067, height: 1600, alt: 'Bouquet of flowers.', loading: 'lazy'}, 
-        {key: 8, highResImage: diningRoom, compressedImage: diningRoomCompressed, width: 1000, height: 1500, alt: "Edie's parent's living room.", loading: 'lazy'}, 
-        {key: 9, highResImage: bedroom1, compressedImage: bedroom1Compressed, width: 933, height: 1400, alt: 'Dog sitting on bed in front of portrait in the background.', loading: 'lazy'}, 
-        {key: 10, highResImage: bedroom2, compressedImage: bedroom2Compressed, width: 1200, height: 1800, alt: 'Empty bedroom with chair and fur rug draped over the chair.', loading: 'lazy'}, 
-        {key: 11, highResImage: nightstand1, compressedImage: nightstand1Compressed, width: 1067, height: 1600, alt: 'Nightstand photo 1.', loading: 'lazy'}, 
-        {key: 12, highResImage: nightstand2, compressedImage: nightstand2Compressed, width: 1333, height: 2000,  alt: 'Nightstand photo 2.', loading: 'lazy'}, 
-        {key: 13, highResImage: NYT, compressedImage: NYTCompressed, width: 1800, height: 1200, alt: 'Copy of the NYT laid out on a dining room table.', loading: 'lazy'} ];
+        {key: 0, highResImage: grammieKitchen, compressedImage: grammieKitchenCompressed, alt: "Edie's grandma's kitchen." }, 
+        {key: 1, highResImage: chineseCabinet, compressedImage: chineseCabinetCompressed, alt: 'Ornate black and gold chinese cabinet.'}, 
+        {key: 2, highResImage: livingRoom1, compressedImage: livingRoom1Compressed, width: 1800, height: 1200, alt: '72 Clermont Ave shot # 1.'}, 
+        {key: 3, highResImage: livingRoom2, compressedImage: livingRoom2Compressed, width: 1200, height: 1800, alt: '72 Clermont Ave shot # 2.'}, 
+        {key: 4, highResImage: livingRoom3, compressedImage: livingRoom3Compressed, width: 1200, height: 1800, alt: '72 Clermont Ave shot # 3.'}, 
+        {key: 5, highResImage: bathroom1, compressedImage: bathroom1Compressed, width: 1300, height: 1950, alt: 'Bathroom shot 1.'}, 
+        {key: 6, highResImage: bathroom2, compressedImage: bathroom2Compressed, width: 1300, height: 1950, alt: 'Bathroom shot 2.'}, 
+        {key: 7, highResImage: flowers, compressedImage: flowersCompressed, width: 1067, height: 1600, alt: 'Bouquet of flowers.'}, 
+        {key: 8, highResImage: diningRoom, compressedImage: diningRoomCompressed, width: 1000, height: 1500, alt: "Edie's parent's living room."}, 
+        {key: 9, highResImage: bedroom1, compressedImage: bedroom1Compressed, width: 933, height: 1400, alt: 'Dog sitting on bed in front of portrait in the background.'}, 
+        {key: 10, highResImage: bedroom2, compressedImage: bedroom2Compressed, width: 1200, height: 1800, alt: 'Empty bedroom with chair and fur rug draped over the chair.'}, 
+        {key: 11, highResImage: nightstand1, compressedImage: nightstand1Compressed, width: 1067, height: 1600, alt: 'Nightstand photo 1.'}, 
+        {key: 12, highResImage: nightstand2, compressedImage: nightstand2Compressed, width: 1333, height: 2000,  alt: 'Nightstand photo 2.'}, 
+        {key: 13, highResImage: NYT, compressedImage: NYTCompressed, width: 1800, height: 1200, alt: 'Copy of the NYT laid out on a dining room table.'} ];
 
     return <Images images={imageArray}/>
 }
