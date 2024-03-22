c0b973214fee7892b0cd3d1712a255046a7de5f3
izzy1090 <michaelisabella1@gmail.com>
2024-03-21 21:30:41 -0400

    Removed an unused useEffect

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import useGlobalStates from "../hooks/use-globalStates";
 
 function Contact ( { contactForms, contactText } ) {
