4e2590f2a185afce6556ea9886dde601f0c03741
izzy1090 <michaelisabella1@gmail.com>
2024-08-08 21:44:28 -0400

    Updating test

---
+++ api/update-token.js
@@ -0,0 +1,14 @@
+import { sql } from '@vercel/postgres';
+ 
+export default async function handler(request, response) {
+  try {
+    // const accessToken = request.query.accessToken;
+    // if (!accessToken) throw new Error('Access token was not included');
+    await sql`UPDATE tokens SET access_token = 6 WHERE id = 0;`;
+  } catch (error) {
+    return response.status(500).json({ error });
+  }
+ 
+  const ig_tokens = await sql`SELECT * FROM tokens;`;
+  return response.status(200).json({ ig_tokens });
+}
