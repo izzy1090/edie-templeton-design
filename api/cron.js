4d0c00a1c6d159d41e2eb7af78e392aba013bd3a
izzy1090 <michaelisabella1@gmail.com>
2024-08-13 01:47:05 -0400

    Revert "Adding an absolute URL to the CRON job"
    This reverts commit a0f9637e9fdfdf7a969dbe0c8bb9be9e338240e9.

--- api/cron.js
+++ api/cron.js
@@ -2,13 +2,5 @@ import exchangeToken from "../src/internal-api/exchange-token.js";
 import refreshToken from "../src/internal-api/refresh-token.js";
 
 export default async function cron(){
-    try {
-        fetch(`https://edietempletondesign.com/api/fetch-sql-token`)
-        .then(response=>response.json())
-        .then(currentToken => refreshToken(currentToken.result.rows[0].access_token))
-        .then(newAccessToken=>fetch(`https://edietempletondesign.com/api/update-sql-token?accessToken=${newAccessToken.access_token}`));
-    }
-    catch (error){
-        console.log(error.message);
-    }
+   exchangeToken(); 
 }
