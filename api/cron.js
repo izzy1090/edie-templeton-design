34dda26834d5bddc739f4fc9d7c1f8b0f17defa5
izzy1090 <michaelisabella1@gmail.com>
2024-08-13 01:27:01 -0400

    testing cron job

---
+++ api/cron.js
@@ -0,0 +1,13 @@
+import refreshToken from "./refresh-token.js";
+
+export default async function exchangeToken(){
+    try {
+        fetch(`/api/fetch-sql-token`)
+        .then(response=>response.json())
+        .then(currentToken => refreshToken(currentToken.result.rows[0].access_token))
+        .then(newAccessToken=>fetch(`/api/update-sql-token?accessToken=${newAccessToken.access_token}`));
+    }
+    catch (error){
+        console.log(error.message);
+    }
+}
