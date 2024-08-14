706ff5d00e765d60f9f65f277b7f91945133c40b
izzy1090 <michaelisabella1@gmail.com>
2024-08-13 21:36:52 -0400

    Moved api endpoints into the API folder

--- api/exchange-token.js
+++ api/exchange-token.js
@@ -2,10 +2,12 @@ import refreshToken from "./refresh-token.js";
 
 export default async function exchangeToken(){
     try {
-        fetch(`/api/fetch-sql-token`)
+        fetch(`https://edietempletondesign.com/api/fetch-sql-token`)
         .then(response=>response.json())
         .then(currentToken => refreshToken(currentToken.result.rows[0].access_token))
-        .then(newAccessToken=>fetch(`/api/update-sql-token?accessToken=${newAccessToken.access_token}`));
+        
+        .then(newAccessToken=>fetch(`https://edietempletondesign.com/api/update-sql-token?accessToken=${newAccessToken.access_token}`))
+        .then(data=>console.log(data));
     }
     catch (error){
         console.log(error.message);
--- src/components/instagram.js
+++ src/components/instagram.js
@@ -1,9 +1,10 @@
-import exchangeToken from "../internal-api/exchange-token.js";
+// import exchangeToken from "../internal-api/exchange-token.js";
 
 function Instagram (){
     return <button 
         style={{ display: 'flex', margin: 'auto' }}
-        onClick={exchangeToken}>
+        // onClick={exchangeToken}
+        >
             Click me!
         </button>
 }
