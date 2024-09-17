9fe55a65d4a7887f24fcac55d37210df42cfce4f
izzy1090 <michaelisabella1@gmail.com>
2024-09-16 22:58:32 -0400

    Wrote two scripts to create and update a table for IG's returned JSON

---
+++ api/create-ig_data-table.js
@@ -0,0 +1,17 @@
+import { sql } from '@vercel/postgres';
+ 
+export default async function handler(
+  request,
+  response,
+) {
+  try {
+    const result =
+        await sql`CREATE TABLE ig_data (
+            id SERIAL PRIMARY KEY,
+            data JSONB
+        );`;
+    return response.status(200).json({ result });
+  } catch (error) {
+    return response.status(500).json({ error });
+  }
+}
