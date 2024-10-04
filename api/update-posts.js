38aab21ec68c1fa4f60ff6f2a50e82a8e331c3ae
izzy1090 <michaelisabella1@gmail.com>
2024-10-03 23:11:52 -0400

    Added script to only add the latest posts to the database

--- api/update-posts.js
+++ api/update-posts.js
@@ -7,14 +7,16 @@ export default async function updatePosts(
 ){
 
     try {
-        const request = await fetchPosts();
-        // const latestTimestamp = await fetch('https://edietempletondesign.com/api/fetch-latest-post-timestamp');
-        // console.log(latestTimestamp)
-        // const latestPostTimestamp = request.data[0].timestamp
-        // check if there is a timestamp in your request results
-            // if not don't make a comparison to instagram's api timestamp
-            // if there is a timestamp
-        for (const post of request.data){
+        const latestPosts = await fetchPosts();
+        const requestLatestTimestamp = await sql`SELECT post_timestamp from ig_data order by post_timestamp::timestamp desc limit 1;`
+        const latestTimestamp = requestLatestTimestamp.rows[0].post_timestamp;
+        const newPosts = [];
+        latestPosts.data.forEach((post)=>{
+            if (post.timestamp > latestTimestamp){
+                newPosts.push(post)
+            }
+        })
+        for (const post of newPosts){
             await sql`INSERT INTO ig_data 
                 (post_id, post_timestamp, post_username, post_caption, post_media_type, post_media_url, post_permalink) 
                 VALUES (${post.id}, ${post.timestamp}, ${post.username}, ${post.caption}, ${post.media_type}, ${post.media_url}, ${post.permalink});`
@@ -27,23 +29,11 @@ export default async function updatePosts(
                 }
             }
             console.log(`record id: ${post.id}, has been added!`)
-            
         };
-        return response.status(200).json({message: `The new instagram posts have been added to the database. Here's a list for reference: ${request} `})
+        if (newPosts.length > 0){
+            return response.status(200).json({newPosts})
+        } else return response.status(200).json({message: "There are no new posts to add to the database."})
     } catch (error) {
         console.log(error)
     }
 }
-
-// Fetch posts from instagram's api once a week (this will be CRON)
-// Firt we return the first set of posts 
-    // we also want to iterate over the paginated posts until there are no more posts to return
-    // this would involve making api calls for each 'next' key value that has a URL to make an HTTP request with
-// Finally we want to store all the posts after they've been returned in vercel's postgreSQL
-
-// could make checks against the timestamp of the last time i made a pull to grab only the latest posts
-
-
-// NOTE: create a script on a separate endpoint to build a clean database with the paginated results
-    // run that script once to build the databse
-    // comment out the code and replace the return statement with a status 400 to prevent people from hurting the databse  
