fcf587536a4b47993f756a6c5066b03a3ddb7e81
Michael Isabella <98562265+izzy1090@users.noreply.github.com>
2025-12-16 23:39:47 -0500

    feat: updated photos on the site (#63)
    * Minor nav menu updates
    
    * Updated contact and about page
    
    * Commiting the code changes
    
    * Pushing up some photos
    
    * More photos
    
    * And more
    
    * More
    
    * a few more
    
    * Hopefully the rest

--- src/pages/NavMenuPage.js
+++ src/pages/NavMenuPage.js
@@ -3,10 +3,10 @@ import NavMenu from "../components/navMenu.js";
 function NavMenuPage() {
 
     const menuItems = [
-        {key: 0, value: 'about', path: '/about', id: 'about'},
-        {key: 1, value: 'gallery', path: '/', id: 'gallery'},
-        {key: 2, value: 'inspiration', path: '/inspiration', id: 'inspiration'},
-        {key: 3, value: 'contact', path: '/contact', id: 'contact'}
+        {key: 0, value: 'About', path: '/about', id: 'about'},
+        {key: 1, value: 'Gallery', path: '/', id: 'gallery'},
+        // {key: 2, value: 'Inspiration', path: '/inspiration', id: 'inspiration'},
+        {key: 3, value: 'Contact', path: '/contact', id: 'contact'}
     ];
 
     return <NavMenu menuItems={menuItems}/>
